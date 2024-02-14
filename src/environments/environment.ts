2bb9ee09490f10b33f58fbd66088da375a189b45
SouzaThalez <selath10@gmail.com>
2024-02-14 16:53:42 -0300

    firebase deploy completed

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig:{
+        apiKey: "AIzaSyAc5PKZ_5tiqKWIjkYdL11y52cCBGst3ww",
+        authDomain: "souzatechportfolio.firebaseapp.com",
+        projectId: "souzatechportfolio",
+        storageBucket: "souzatechportfolio.appspot.com",
+        messagingSenderId: "741999960757",
+        appId: "1:741999960757:web:b3ee956e37d7c36d7754fc",
+        measurementId: "G-L0NMZGK7F9"
+    }
+}
